ca4642e59d7c5370739aa3229ee0cf015549e465
Edwin Porras A <edwin.porras@gmail.com>
2023-05-13 08:10:30 -0500

    Carga Inicial

---
+++ src/main/java/com/ejercicios/ejerciciojava03/EjercicioJava03.java
@@ -0,0 +1,11 @@
+package com.ejercicios.ejerciciojava03;
+/**
+ *
+ * @author edwin
+ */
+public class EjercicioJava03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
